0b31a5261e74dcc85ca72b9c5f76abb7b47e27a9
Lyubomila Petkova <Lyubomila.Petkova@KPMG.co.uk>
2021-01-22 19:33:48 +0000

    Add class genre-row

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,6 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $("select").formSelect();
     $(".modal").modal();
-    $(".genre-name div").hide();
-    $(".genre-name div:lt(6)").show();
+    $(".genre-row div").hide();
+    $(".genre-row div:lt(7)").show();
 });
